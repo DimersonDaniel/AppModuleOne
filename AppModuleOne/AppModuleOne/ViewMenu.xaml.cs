4eb53334e379c82b16559d6dace032ce406aaa96
DimersonDaniel <dimerson.daniel@gmail.com>
2019-12-25 19:25:50 -0300

    upload

---
+++ AppModuleOne/AppModuleOne/ViewMenu.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppModuleOne
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ViewMenu : ContentView
+	{
+		public ViewMenu ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
